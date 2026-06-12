ec661f465c892bb68370dc1b2b98caae250be180
Ronei Martins Fonseca <ronei3271@gmail.com>
2026-06-11 23:20:32 -0300

    Remove duplicate import statements in gallery.js

--- quanton3dbackend/routes/gallery.js
+++ quanton3dbackend/routes/gallery.js
@@ -3,12 +3,7 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 import jwt from 'jsonwebtoken';
- codex/revise-code-to-identify-errors-y4xdq2
 
- codex/revise-code-to-identify-errors-s1mcto
-
-codex/revise-code-to-identify-errors-9rfnjr
- main
  main
 import {
   aprovarGalleryItem,
@@ -17,10 +12,6 @@ import {
   listarGalleryItemsAdmin,
   recusarGalleryItem,
 } from '../controllers/galleryController.js';
-codex/revise-code-to-identify-errors-y4xdq2
-
- codex/revise-code-to-identify-errors-s1mcto
-
 
 import { criarGalleryItem, listarGalleryItems, aprovarGalleryItem } from '../controllers/galleryController.js';
  main
